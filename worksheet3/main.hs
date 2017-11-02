65875626b48932f6592ff782bd2525927482e67f
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2017-11-02 08:58:55 +0000

    Initial commit

---
+++ worksheet3/main.hs
@@ -0,0 +1,32 @@
+module Main where
+import Prelude hiding ((&&))
+
+
+--Exercise 1
+(&&) :: Bool -> Bool -> Bool
+(&&) True True = True
+(&&) _ _ = False
+
+--Exercise 2
+--Exercise 3
+--Exercise 4
+--Exercise 5
+--Exercise 6
+--Exercise 7
+--Exercise 8
+--Exercise 9
+--Exercise 10
+--Exercise 11
+--Exercise 5 Version 2
+--Exercise 6 Version 2
+--Exercise 7 Version 2
+--Exercise 8 Version 2
+--Exercise 9 Version 2
+--Exercise 10 Version 2
+
+
+main = do
+	print $ True && True
+	print $ False && True
+	putStrLn "EOP"
+	
