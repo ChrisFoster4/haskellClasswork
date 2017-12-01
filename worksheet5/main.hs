ad97e216673006609ca0364bd705f07edb7502e8
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2017-12-01 10:52:24 +0000

    All ex's done

--- worksheet5/main.hs
+++ worksheet5/main.hs
@@ -4,14 +4,15 @@ import qualified Data.List(isSubsequenceOf) --Used in ex13
 --Return the val of head + 1
 headPlusOne :: [Int] -> Int
 headPlusOne [] = 0
-headPlusOne [n,_] = (head [n] + 1) --TODO make this only two lines
+headPlusOne [n,_] = (head [n] + 1) --TODO Could this only be two lines?
 headPlusOne [n] = (head [n] + 1)
 
 
 --Exercise 2
 --Add the value at the start of the list before the list
 duplicateHead :: [Int] -> [Int]
-duplicateHead list = head list:list --TODO needs to return [] for [] as input
+duplicateHead [] = []
+duplicateHead list = head list:list 
 
 
 --Exercise 3
@@ -26,16 +27,16 @@ listLength (x:xs) = 1 + listLength (xs)
 listLength n = 0
 
 --Exercise 5
+multAllAlt :: [Int] -> Int
+multAllAlt list = product list
+
 multAll :: [Int] -> Int
 multAll (x:xs) = x * multAll xs
 multAll [] = 1
 
-multAllAlt :: [Int] -> Int
-multAllAlt list = product list
-
 --Exercise 6
 andAllAlt :: [Bool] -> Bool
-andAllAlt list = if elem False list then False else True --Is there a better way to reverse this
+andAllAlt list = if elem False list then False else True 
 
 andAll :: [Bool] -> Bool
 andAll (x:xs) = if x == False then False else andAll xs
@@ -43,14 +44,12 @@ andAll [] = True
 
 --Exercise 7
 --return the number of times the first argument appears in the list
---countElems :: Int -> [Int] -> Int
---countElems num list = 
+countElemsAlt :: Int -> [Int] -> Int
+countElemsAlt num list = length $ filter (==num) list
+
 countElems :: Int -> [Int] -> Int
 countElems  y (x:xs) = if x == y then 1 + (countElems y xs) else countElems y xs
 countElems y [] = 0 --I expected this to make it always return 0.
-
-countElemsAlt :: Int -> [Int] -> Int
-countElemsAlt num list = length $ filter (==num) list
 
 --Exercise 8
 removeAllAlt :: Int -> [Int] -> [Int]
@@ -62,19 +61,38 @@ removeAll num [] = []
 
 --Exercise 9
 type StudentMark = (String,Int)
---listMarks student [(x,xs)] = if fst x == student then snd x : (listMarks student xs)else listMarks student xs
 listMarks :: String -> [(String,Int)] -> [Int]
 listMarks student (x:xs) = if fst x == student then snd x : (listMarks student xs) else listMarks student xs
 listMarks student [] = []
 
 --Exercise 10
+--Check if the first argument is a prefix to the second argument
+prefix :: [Int] -> [Int] -> Bool
+prefix [] (_) = True
+prefix (y:ys) (x:xs)= if y /= x then False else prefix ys xs
 
 --Exercise 11
---sunsequence :: [Int] -> [Int] -> Bool
 subsequenceAlt :: [Int] -> [Int] -> Bool
 subsequenceAlt list list2 = list `Data.List.isSubsequenceOf` list2
 
+subsequences :: [Int] -> [Int] -> Bool
+subsequences (y:ys) (x:xs) = if y == x then subsequences ys xs else subsequences (y:ys) xs
+subsequences [] (x:xs) = True 
+subsequences (_) (_) = False 
 
-main =do
-	print $ listMarks "Joe" [("Joe", 45), ("Sam", 70), ("Joe", 52)] 
+
+
+
+main = do
+	--print $ listMarks "Joe" [("Joe", 45), ("Sam", 70), ("Joe", 52)] 
+	--print $ prefix [1,4] [1,4,9,2]
+	--print $ prefix [1,4,3] [1,4,9,2]
+	putStrLn "Should be True"
+	print $ subsequences [1,4,9] [8,1,4,9,2,1]
+	print $ subsequences [8,1,4] [8,1,4,9,2,1]
+	print $ subsequences [4,9,2] [8,1,4,9,2,1]
+	putStrLn "Should be False"
+	print $ subsequences [1,4,9,1] [8,1,4,9,2,1]
+	print $ subsequences [8,2,4] [8,1,4,9,2,1]
+	print $ subsequences [8,5,4] [8,1,4,9,2,1]
 	putStrLn "EOP"
