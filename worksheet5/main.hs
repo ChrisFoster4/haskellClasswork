3d00ff172248f7332445e632b93e8447d444d548
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2017-12-06 09:09:56 +0000

    Subsequences and prefix ex's fixed

--- worksheet5/main.hs
+++ worksheet5/main.hs
@@ -5,19 +5,19 @@ import qualified Data.List(isSubsequenceOf) --Used in ex13
 headPlusOne :: [Int] -> Int
 headPlusOne [] = 0
 headPlusOne [n,_] = (head [n] + 1) --TODO Could this only be two lines?
-headPlusOne [n] = (head [n] + 1)
+headPlusOne [n] = (head [n] + 1) --Non exhaustive pattern if line removed.
 
 
 --Exercise 2
 --Add the value at the start of the list before the list
 duplicateHead :: [Int] -> [Int]
 duplicateHead [] = []
-duplicateHead list = head list:list 
+duplicateHead list = head list:list
 
 
 --Exercise 3
 rotate :: [a] -> [a]
-rotate (x:y:xs) = y:x:xs 
+rotate (x:y:xs) = y:x:xs
 rotate n = n
 
 --Exercise 4
@@ -36,7 +36,7 @@ multAll [] = 1
 
 --Exercise 6
 andAllAlt :: [Bool] -> Bool
-andAllAlt list = if elem False list then False else True 
+andAllAlt list = if elem False list then False else True
 
 andAll :: [Bool] -> Bool
 andAll (x:xs) = if x == False then False else andAll xs
@@ -70,28 +70,35 @@ listMarks student [] = []
 prefix :: [Int] -> [Int] -> Bool
 prefix [] (_) = True
 prefix (y:ys) (x:xs)= if y /= x then False else prefix ys xs
+prefix (y) (x) = y == x
 
 --Exercise 11
+subsequences :: [Int] -> [Int] -> Bool
+subsequences (y:ys) (x:xs) = if prefix (y:ys) (x:xs) then True else subsequences (y:ys) (xs)
+subsequences (y:ys) [] = False
+
 subsequenceAlt :: [Int] -> [Int] -> Bool
 subsequenceAlt list list2 = list `Data.List.isSubsequenceOf` list2
 
-subsequences :: [Int] -> [Int] -> Bool
-subsequences (y:ys) (x:xs) = if y == x then subsequences ys xs else subsequences (y:ys) xs
-subsequences [] (x:xs) = True 
-subsequences (_) (_) = False 
+subsequencesBroken :: [Int] -> [Int] -> Bool
+subsequencesBroken (y:ys) (x:xs) = if y == x then subsequences ys xs else subsequences (y:ys) xs
+subsequencesBroken [] (x:xs) = True
+subsequencesBroken (_) (_) = False
 
 
 
 
 main = do
-	--print $ listMarks "Joe" [("Joe", 45), ("Sam", 70), ("Joe", 52)] 
+	--print $ listMarks "Joe" [("Joe", 45), ("Sam", 70), ("Joe", 52)]
 	--print $ prefix [1,4] [1,4,9,2]
 	--print $ prefix [1,4,3] [1,4,9,2]
 	putStrLn "Should be True"
+	print $ subsequences [1,4,9] [8,1,4,2,1,4,9] --Broken
 	print $ subsequences [1,4,9] [8,1,4,9,2,1]
 	print $ subsequences [8,1,4] [8,1,4,9,2,1]
 	print $ subsequences [4,9,2] [8,1,4,9,2,1]
 	putStrLn "Should be False"
+	print $ subsequences [1,4,2,1,4,9,1] [8,1,4,9,2,1]
 	print $ subsequences [1,4,9,1] [8,1,4,9,2,1]
 	print $ subsequences [8,2,4] [8,1,4,9,2,1]
 	print $ subsequences [8,5,4] [8,1,4,9,2,1]
